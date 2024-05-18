96944d7f8211bf50891c021ffdd9658823b2d409
Nithin Mahesh <nmahesh.informatics@gmail.com>
2024-05-18 15:23:07 -0400

    This is an example from openAI docs for uploading one image - works well to describe whats in image - need to expand with multiple images

--- src/chatAPI.py
+++ src/chatAPI.py
@@ -1,3 +1,47 @@
+import requests
+import base64
+
 def main():
-    print("chat")
+    api_key = input("Enter your openAI api key: \n")
+    imagePath = ""
+
+    # Getting the base64 string
+    base64_image = encode_image(imagePath)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+    "model": "gpt-4o",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": "What’s in this image?"
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+            }
+        ]
+        }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    print(response.json())
+
+
+
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
